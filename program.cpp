b7bf2ccf6f2aa2f39465bbd7b4d201195cf782e6
Stefanija <sstefani.321@gmail.com>
2019-04-04 10:29:40 +0200

    functions implemented

--- program.cpp
+++ program.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout<<"Stefanija Zdraveska"<<endl;
 }
 
 void printMyIndex () {
 	//TODO
+	cout<<"173157"<<endl;
 }
 
 int main () {
 	//TODO
+	printMyName();
+	printMyName();
+	
 }
